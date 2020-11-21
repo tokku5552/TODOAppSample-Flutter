bf940f025f5594cc189fe1aa4424162af3f80dd1
tokku5552 <physics.0121@gmail.com>
2020-11-22 01:30:10 +0900

    一覧の更新処理変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:sqflite/sqflite.dart';
 import 'package:todo_app_sample_flutter/main_model.dart';
+import 'package:todo_app_sample_flutter/todo_item_detail_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,26 +10,42 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'TodoAppSampleFlutter',
-      home: ChangeNotifierProvider<MainModel>(
-        create: (_) => MainModel(),
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Todo App Sample Flutter"),
-          ),
-          body: Consumer<MainModel>(builder: (context, model, child) {
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    model.test,
+    return MaterialApp(title: 'TodoAppSampleFlutter', home: MainPage());
+  }
+}
+
+class MainPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<MainModel>(
+      create: (_) => MainModel()..getTodoList(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Todo App Sample Flutter"),
+        ),
+        body: Consumer<MainModel>(builder: (context, model, child) {
+          final todoList = model.list;
+          return ListView(
+            children: todoList
+                .map(
+                  (todo) => ListTile(
+                    title: Text(todo.title),
                   ),
-                ],
+                )
+                .toList(),
+          );
+        }),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => TodoItemDetailPage(),
+                fullscreenDialog: true,
               ),
             );
-          }),
+          },
+          child: Icon(Icons.add),
         ),
       ),
     );
--- lib/main_model.dart
+++ lib/main_model.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app_sample_flutter/todo_item.dart';
+import 'package:todo_app_sample_flutter/todo_item_repository.dart';
 
 class MainModel extends ChangeNotifier {
   List<TodoItem> list = [];
-  String test = 'test';
 
   void getTodoList() async {
+    list = await TodoItemRepository.getAll();
     notifyListeners();
   }
 }
