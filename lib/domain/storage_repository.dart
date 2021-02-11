a14125faf1bafc5068f12c64daf2e71d2f5cb9ea
tokku5552 <physics.0121@gmail.com>
2021-02-11 13:42:38 +0900

    fix:add lisence

--- lib/domain/storage_repository.dart
+++ lib/domain/storage_repository.dart
@@ -1,7 +1,15 @@
+/*
+ * Copyright (c) 2021 tokku5552
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/mit-license.php
+ *
+ */
 abstract class StorageRepository {
   Future<void> savePersistenceStorage(String key, String value);
   Future<String> loadPersistenceStorage(String key);
   Future<bool> isExistKey(String key);
 }
+
 const String VIEW_COMPLETED_ITEMS_KEY = "view_completed_items";
 const String VIEW_COMPLETED_ITEMS_KEY_NONE = "NONE";
